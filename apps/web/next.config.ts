65277044364fb9290c04a067c919cd568dc1d25a
nyranmoodie <nyranmoodie@gmail.com>
2026-07-22 08:17:11 -0500

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@just-us/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
